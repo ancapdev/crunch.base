a02511f74f0995e0af60a653a9e08269128309b3
Christian Rorvik <christian.rorvik@gmail.com>
2011-12-03 22:37:13 +0000

    Added CRUNCH_MUST_CHECK_RESULT macro, and IgnoreResult helper

---
+++ include/crunch/base/ignore_result.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2011, Christian Rorvik
+// Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
+
+#ifndef CRUNCH_BASE_IGNORE_RESULT_HPP
+#define CRUNCH_BASE_IGNORE_RESULT_HPP
+
+namespace Crunch {
+
+template<typename T> void IgnoreResult(T) {}
+
+}
+
+#endif
---
+++ include/crunch/base/must_check_result.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2011, Christian Rorvik
+// Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
+
+#ifndef CRUNCH_BASE_MUST_CHECK_RESULT_HPP
+#define CRUNCH_BASE_MUST_CHECK_RESULT_HPP
+
+#include "crunch/base/platform.hpp"
+
+#if defined (CRUNCH_COMPILER_GCC)
+#   define CRUNCH_MUST_CHECK_RESULT __attribute__((warn_unused_result))
+#else
+#   define CRUNCH_MUST_CHECK_RESULT
+#endif
+
+#endif
